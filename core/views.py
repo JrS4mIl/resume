12a98cc52b7e255e0868c155329317e9af4676a3
Samil Yesil <ysamil072@gmail.com>
2023-10-04 18:59:56 +0300

    refactor: general setting create

--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    site_title=GeneralSetting.objects.get(name='site_title').paramater
+    site_keywords=GeneralSetting.objects.get(name='site_keywords').paramater
+    site_description=GeneralSetting.objects.get(name='site_description').paramater
+
+    home_banner_name=GeneralSetting.objects.get(name='home_banner_name').paramater
+    home_banner_title=GeneralSetting.objects.get(name='home_banner_title').paramater
+    home_banner_description=GeneralSetting.objects.get(name='home_banner_description').paramater
+
+    about_myself=GeneralSetting.objects.get(name='about_myself').paramater
+    about_footer = GeneralSetting.objects.get(name='about_footer').paramater
+    context={
+        'site_title':site_title,
+        'site_keywords':site_keywords,
+        'site_description':site_description,
+        'home_banner_name':home_banner_name,
+        'home_banner_title':home_banner_title,
+        'home_banner_description':home_banner_description,
+        'about_myself':about_myself,
+        'about_footer':about_footer,
+    }
+    return render(request,'index.html',context)
 
